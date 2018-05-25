d345dd988f72dc977a6a156a2036b314e3179294
Matthew Rosenberg <mhrosenberg@caltech.edu>
2018-05-25 01:07:48 -0700

    first commit savePSDtoPDF

---
+++ analyzeThese_magneto.py
@@ -0,0 +1,286 @@
+import glob
+import matlab.engine # conflicts with conda therefore imported below
+import os.path
+import os
+import bashrcMod # don't forget that importing a file executes it!
+# import reinstateCondaPathToBashrc
+# import removeCondaPathFromBashrc
+
+### temp parameters DO NOT POPULATE!
+# rawMDApath = ''
+# prePath = ''
+# PATH_a_Analysis = ''
+# rawMDApaths = ''
+###
+
+def removeCondaPathFromBashrc(BASHRC_DIR):
+    commentOutCondaPathInBashrc = "sed -i 's/export PATH=\"\/home\/matthew\/miniconda3\/bin:\$PATH\"/\#export PATH=\"\/home\/matthew\/miniconda3\/bin:\$PATH\"/g' ./.bashrc"
+    os.chdir(BASHRC_DIR)
+    os.system(commentOutCondaPathInBashrc)
+    os.system('. ~/.bashrc')
+    print(BASHRC_DIR + '.bashrc modified to remove conda path as follows: ')
+    print(commentOutCondaPathInBashrc)
+
+def reinstateCondaPathInBashrc(BASHRC_DIR):
+    reinstateCondaPathInBashrc = "sed -i 's/\#export PATH=\"\/home\/matthew\/miniconda3\/bin:\$PATH\"/export PATH=\"\/home\/matthew\/miniconda3\/bin:\$PATH\"/g' ./.bashrc"
+    os.system(reinstateCondaPathInBashrc)
+    os.system('. ~/.bashrc')
+    print(BASHRC_DIR + '.bashrc modified to reinstate conda path as follows: ')
+    print(reinstateCondaPathInBashrc)
+
+### recursive search 
+### stores results in pathList (don't forget to clear results in pathList before reusing)
+def recursiveSearch(exten): 
+    # The top argument for walk. Th
+    topdir = '.'
+    # Start the walk
+    os.path.walk(topdir, step, exten)
+    return pathList
+def step(ext, dirname, names):
+    ext = ext.lower()
+ 
+    for name in names:
+        if name.lower().endswith(ext):
+            tmpPath = os.path.join(dirname, name) 
+            pathList.append(tmpPath)
+### end method group
+
+def saveRawDataPathsToTxt(pathList):
+    pathFile = open('rawDataPaths.txt', 'w')
+    for path in pathList:
+        cwd = os.getcwd()
+        # path = path[]
+        path = path.replace(path[:2],cwd + '/')
+        path = path.replace('/Continuous_Data.openephys','')
+        pathFile.write("%s\n" % path)
+    pathFile.close()     
+
+def generateRecordingIDsToRun():
+    recIDlist = []
+    with open('rawDataPaths.txt') as paths:
+        for recording in paths:
+            recording = recording.strip() # remove new line
+            slashInd = recording.rfind('/')
+            recID = recording[slashInd+1:]
+            recIDlist.append(recID)
+
+    recIDlistFile = open('recordingIDsToRun.txt', 'w')
+    for recID in recIDlist:
+        recIDlistFile.write("%s\n" % recID)
+    recIDlistFile.close()
+    return recIDlist
+
+def convertOEtoMDA():
+    ### mountain sort loop
+    eng = matlab.engine.start_matlab()
+    eng.addpath(PATH_a_Analysis)
+    os.chdir(PATH_rawParent)
+    with open('recordingIDsToRun.txt') as recIDs, open('rawDataPaths.txt') as rawPaths:
+        recIDs = recIDs.read().splitlines()
+        rawPaths = rawPaths.read().splitlines()
+        for ind in range(0,len(recIDs)):
+            saveLocation = '../../spikesorting/mountainSorted/' + recIDs[ind].strip() #+
+            if not os.path.exists(saveLocation):
+                os.makedirs(saveLocation)
+                
+                print('created mountainSort dir: ' + saveLocation)
+                print('converting open ephys to MDA format')
+
+                eng.workspace['BASE_FILE_PATH'] = rawPaths[ind].strip() 
+                eng.workspace['RAW_MDA_OUTPUT_NAME'] = recIDs[ind].strip()
+                eng.workspace['RAW_MDA_SAVE_PATH'] = saveLocation
+                eng.workspace['CHAN_NUM'] = CHAN_NUMS
+                eng.convertToMDA_1_30_18(eng.workspace['BASE_FILE_PATH'], 
+                    eng.workspace['RAW_MDA_OUTPUT_NAME'], eng.workspace['RAW_MDA_SAVE_PATH'], 
+                    eng.workspace['CHAN_NUM'])
+            else:
+                print(saveLocation + ' already exists --> skipping this conversion')
+
+def getRawMDApaths():
+    os.chdir(PATH_rawParent)
+    os.chdir('../../spikesorting/mountainSorted/')
+    # print('cd' + os.getcwd())
+    MDA_parentPath = os.getcwd() + '/'
+
+    pathList = recursiveSearch('raw.mda')
+    rawMDApaths = []
+    for MDApath in pathList:
+        MDApath = MDApath.replace('./', MDA_parentPath)
+        MDApath = MDApath.strip()
+        rawMDApaths.append(MDApath)
+    return rawMDApaths
+
+def runMntSrt(rawMDApaths):
+    # removeCondaPathFromBashrc() # anaconda path in bashrc breaks mountainSort but is necessary for phy (kilosort visualization)
+    os.chdir(PATH_rawParent)
+    print('cd into ' + os.getcwd())
+    with open('recordingIDsToRun.txt') as recIDs:
+        recIDs = recIDs.read().splitlines()
+        os.chdir('../../spikesorting/a_paramsNconfigs/mountainSorting')
+        print('running mountainSort commands from' + os.getcwd() + '\n' + 'running mountain sort now')
+        print('reminder: are you passing the correct probe geom.csv file based on the probe type?')
+        for recID in recIDs:
+            for rawMDApath in rawMDApaths:
+                if recID in rawMDApath: 
+                    print('\nspike sorting: \n' + rawMDApath) 
+                    rawMDAdir = os.path.dirname(rawMDApath)
+
+                    mntSrt_run_options = 'mlp-run mountainsort3.mlp sort --raw=' + rawMDApath + ' --geom=' + PROBE_GEOM_CSV_NAME + '.csv --firings_out=firings_curated.mda --firings_original_out=firings_original.mda --filt_out=filt.mda --pre_out=pre.mda --cluster_metrics_out=metrics.json --_params=params.json --curate=true' 
+                    os.system(mntSrt_run_options)
+                    mntSort_saveDir = os.path.dirname(rawMDApath) + '/'
+                    os.system('mv *.mda metrics.json ' + mntSort_saveDir)
+                    os.system('cp *.csv *.mlp *params.json ' + mntSort_saveDir)
+
+    # reinstateCondaPathInBashrc()    
+
+## WIP: 
+# def viewMntSrt(recIDs, mntSrt_run_options):
+#     os.chdir(PATH_rawParent)
+#     with open('recordingIDsToRun.txt') as recIDs: # WIP: change to recordingIDsToView.txt
+#         recIDs = recIDs.read().splitlines()
+#         for recID in recIDs:
+#             for rawMDApath in rawMDApaths:
+#                 if recID in rawMDApath: 
+#                     print('\nspike sorting: \n' + rawMDApath) 
+#                     os.chdir(PATH_a_Analysis)
+#                     print('cd into' + os.getcwd() + '\n' + 'running mountain sort now') 
+#                     rawMDAdir = os.path.dirname(rawMDApath)
+#                     prePath = rawMDAdir + '/' + recID
+#                     print(prePath)
+#                     os.chdir(prePath)
+#                     print('cd into ' + os.getcwd())
+#                     print(rawMDApath)
+#                     # '.mlp sort --raw=' + rawMDApath + ' --geom=geom.csv --firings_out=' + prePath + ' _firings_curated.mda --firings_original_out=' + prePath + '_firings_original.mda --filt_out=' + prePath + '_filt.mda --pre_out=' + prePath + '_pre.mda --cluster_metrics_out=' + prePath + '_metrics.json --_params=' + PATH_a_Analysis + '/params.json --curate=true' 
+#                     mntSrt_view_options = 'mountainview --raw=' + rawMDApath + ' --filt=filt.mda --pre=pre.mda --geom=geom.csv --firings=firings_curated.mda --cluster_metrics=metrics.json'                    
+#                     os.system(mntSrt_view_options)
+
+def runKiloSort():
+    os.chdir(PATH_rawParent)
+    eng = matlab.engine.start_matlab()
+    with open('recordingIDsToRun.txt') as recIDs, open('rawDataPaths.txt') as rawPaths:
+        recIDs = recIDs.read().splitlines()
+        rawPaths = rawPaths.read().splitlines()
+        print('\nPreparing to sort: ')
+        print('\n'.join(recIDs))
+        print('\namong the following (possible) raw data in subdirs of the cwd: ')
+        print('\n'.join(rawPaths))
+        
+        for recID in recIDs:
+            for rawPath in rawPaths:
+                if recID in rawPath:
+                    os.chdir(PATH_rawParent)
+                    try:
+                    	os.chdir('../../spikesorting/kiloSorted')
+                    except:
+                    	print('\n dir not found, therefore creating dir: ../../spikesorting/kiloSorted')
+                    	os.chdir('../../')
+                    	os.system('mkdir spikesorting/kiloSorted') # UNTESTED: can this make a nested directory?
+                    kiloDir = os.getcwd()
+                    kiloSaveDir = kiloDir + '/' + recID
+                    if not os.path.exists(kiloSaveDir):
+                        os.chdir(rawPath)
+
+                        if not os.path.exists('./100_CH1.continuous'):
+                        	os.system('cp 100_ch1.continuous ./100_CH1.continuous') # alternatively: os.system("rename 's/ch/CH/' *.continuous")
+                        	print('copied continuous OE file from ch1 to CH1')
+                        else:
+                        	print('data already named correctly; no need to rename OE file from ch1 to CH1')
+                        os.chdir(PATH_rawParent)
+                        os.chdir('../../spikesorting/a_paramsNconfigs/kiloSorting/')
+                        eng.addpath(os.getcwd())
+
+                        if os.path.exists(PROBE_CHAN_MAP_PATH):
+                        	eng.workspace['CHAN_MAP_MAT_PATH'] = PROBE_CHAN_MAP_PATH
+                        else:
+                        	print('ERROR: ' + PROBE_CHAN_MAP_PATH + 'does not exist')
+                		print('reminder: have you checked that the parameters in the separate matlab files are correct? \nLook in: ' + PROBE_CHAN_MAP_PATH)
+
+                        eng.workspace['RAW_DATA_ROOT_PATH'] = rawPath.strip()
+
+                        if POSTHOC_MERGE == 'yes':
+                            eng.master_file_2_9_18posthocMerged(nargout = 0)
+                            kiloSaveDir = kiloSaveDir + 'posthocMerged'
+                        elif POSTHOC_MERGE == 'no':
+                            eng.master_file_2_5_18(nargout = 0)
+                        
+                        os.makedirs(kiloSaveDir)
+                        os.system('mv ' + rawPath + '/kilosort.dat ' + kiloSaveDir)
+                        os.system('mv ' + rawPath + '/*.npy ' + kiloSaveDir)
+                        os.system('mv ' + rawPath + '/params.py ' + kiloSaveDir)
+                        os.system('mv ' + rawPath + '/rez.mat ' + kiloSaveDir)
+                    else:
+                        print('\nsorting results already exist for: ' + recID)
+                        print('rename, move, or delete this dir to rerun the analysis of this dataset')
+    print('\nfinished with kiloSorting')
+
+### MAIN CODE: 
+
+## common parameters:
+CHAN_NUMS = 64
+SPIKESORTER = 'mountainSort' # use 'kiloSort', mountainSort', 'none', or 'both'
+SORT_ALL_IN_SUBDIRS = 'yes' # 'yes' or 'no' # no requires a pre-populated recordingIDsToRun.txt file in the cwd of this script 
+PATH_a_Analysis = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis'
+PATH_rawParent = os.getcwd()
+BASHRC_DIR = '/home/matthew'
+
+## kiloSort specific parameters:
+PROBE_CHAN_MAP_PATH = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis/spikeSorting/kiloSorting/probe64F_chanMap.mat' #make this file using createChannelMapFile.m       
+POSTHOC_MERGE = 'no' # 'yes' or 'no' yes merges together clusters kilosort suspects as being the same etc
+
+## mountainSort specific parameters
+PROBE_GEOM_CSV_NAME = 'geom64F' # DO NOT ADD THE .csv file ext
+
+### CHOOSING DATA: output = recordingIDsToRun.txt
+## get all data paths for subdirs in ./
+pathList = []    
+if SORT_ALL_IN_SUBDIRS == 'yes':
+    print('sorting all in subdirs from the location of this script')
+    exten = '.openephys' # make this a UNIQUE FILE within the raw open ephys dir
+
+    recursiveSearch(exten)
+    saveRawDataPathsToTxt(pathList)
+    ### comment out and modify recordingIDsToRun.txt manually to run on a subset of the raw data in the child dirs of this directory
+    recIDlist = generateRecordingIDsToRun() 
+    print('\n'.join(recIDlist))
+    print('\nremove some of the above from recordingIDsToRun.txt to run on a subset of the child dir data\n')
+elif SORT_ALL_IN_SUBDIRS == 'no':
+  print('sorting based on contents of recordingIDsToRun.txt')
+#   ## WIP check for file existence
+else:
+    print('error: invalid selection for SORT_ALL_IN_SUBDIRS param')
+
+
+### SPIKE SORTING
+print('using spikesorter: ' + SPIKESORTER) ########## WIP: programmatically solve conda bashrc conflict b/w kilosort and mntSort
+if SPIKESORTER == 'kiloSort':
+    print('spikesorting with kilosort')
+    runKiloSort()
+elif SPIKESORTER == 'mountainSort':
+    ### FILE TYPE CONVERSION: only needs to be run the first time
+    convertOEtoMDA()
+    ## searches through all MDA files in this project dir tree for matches with recordingIDsToRun.txt
+    pathList = [] # REQUIRED FOR getRawMDApaths() !!!
+    rawMDApaths = getRawMDApaths()
+    print('\nraw MDA paths: \n')
+    print('\n'.join(rawMDApaths))
+
+    runMntSrt(rawMDApaths) # WIP: make param for mntSrt_run_options 
+elif SPIKESORTER == 'both':
+    #### mountainSort
+    ### FILE TYPE CONVERSION: only needs to be run the first time
+    convertOEtoMDA()
+    ## searches through all MDA files in this project dir tree for matches with recordingIDsToRun.txt
+    pathList = []
+    rawMDApaths = getRawMDApaths()
+    print(rawMDApaths)
+    runMntSrt(rawMDApaths) # WIP: make param for mntSrt_run_options 
+    #### kiloSort
+    print('spikesorting with kilosort')
+    runKiloSort()
+elif SPIKESORTER == 'none':
+    print('you chose not to spikesort')
+else:
+    print('invalid sorter specified as parameter. use kiloSort or mountainSort')
+
+### VIEW RESULTS
+# reinstateCondaPathInBashrc(BASHRC_DIR)
---
+++ savePSDtoPDF.py
@@ -0,0 +1,315 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import style
+from matplotlib import pyplot as plt 
+from matplotlib.backends.backend_pdf import PdfPages
+import math
+import sys
+sys.path.insert(0, '/media/matthew/Data/gitResearch/OpenEphysAnalysisTools/Python3/OpenEphys.py') # TO DO: get the import folder thing to work with a path to local open ephys github folder
+import OpenEphys as OE
+import os
+import glob
+import time
+import datetime
+import webbrowser
+from pathlib import Path, PureWindowsPath # only works on 3.4+ (not 2.7)
+import platform
+
+### CHECK USER PARAMETERS THAT FOLLLOW THESE FUNCTIONS!
+
+def genericInvalidParamError(userParamStr):
+	return print('ERROR: You provided an invalid user parameter! Correct this param: ' + userParamStr)
+
+def loadRawOEdir(rawDataDir):
+	# print('loading: ' + pathToFile)
+	# data_dict = OE.load(pathToFile) # returns a dict with data, timestamps, etc.
+	# print(data_dict)
+
+	# # open ephys's BROKEN (float given but expects int error) downsample code
+	# # def downsample(trace,down):
+	# #     downsampled = scipy.signal.resample(trace,np.shape(trace)[0]/down)
+	# #     return downsampled
+	data = OE.loadFolderToArray(rawDataDir, channels = range(FIRST_CH, LAST_CH+1), chprefix = 'CH', dtype = float, session = '0', source = '100')
+	print(np.shape(data))
+	totalNumSamples = np.shape(data)[0] #no longer used but available for later
+	print(data)
+	return data
+
+def saveRawDataPathsToTxt(pathList):
+	ts = time.time()
+	timeStr = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d__%H_%M_%S')
+	inputDataPathsTextFileName = 'inputDataPaths' + timeStr + '.txt'
+	with open(inputDataPathsTextFileName, 'w') as pathFile:
+		for path in pathList:
+			# cwd = os.getcwd()
+			# path = path.replace(path[:2],cwd + '/')
+			path = path.replace('/Continuous_Data.openephys','')
+			pathFile.write("%s\n" % path)
+	print('these paths are saved rawDataPaths.txt in the cwd')
+
+def generateRecordingIDsToRun():
+	recPathlist = []
+	with open('rawDataPaths.txt') as paths:
+		for recording in paths:
+			recording = recording.strip() # remove new line
+			slashInd = recording.rfind('/')
+			recID = recording[slashInd+1:]
+			recPathlist.append(recID)
+
+	recIDlistFile = open('recordingIDsToRun.txt', 'w')
+	for recID in recPathlist:
+		recIDlistFile.write("%s\n" % recID)
+	recIDlistFile.close()
+	return recPathlist
+
+### INPUT DATA PARAMETERS:
+### choose among: 'mostRecent' # 'singleDir' 'singleFile' 'allInChildDirs' 
+DATA_SELECTION_SCHEME = 'allInChildDirs' # 'singleDir' 'singleFile' 'allInChildDirs' 'fromTextFileInCwd' 
+EPHYS_PARENT_PATH = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Data/raw/noiseTests/' #use '' for current work directory
+SUMMARY_OUTPUT_PATH = os.getcwd() # or modify to custom dir 
+PATH_a_Analysis = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis'
+PATH_rawParent = os.getcwd()
+PATH_a_Analysis = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis'
+PATH_rawParent = os.getcwd()
+BASHRC_DIR = '/home/matthew'
+
+### ANALYSIS PARAMETERS:
+NUM_CH = 64
+SAMPLE_RATE_HZ = 30000
+FIRST_CH = 1 # 1 indexed
+LAST_CH = 64
+INSPECT_INDIVIDUAL_CHAN = 0 # 0 for no; 1 for yes
+INSPECT_ONE_BY_ONE = 0
+SAVE_ALL_TO_PDF = 1
+ 
+###data selection 
+print('\nDATA_SELECTION_SCHEME: ' + DATA_SELECTION_SCHEME)
+if DATA_SELECTION_SCHEME == 'mostRecent':
+	latest_subdir = max(glob.glob(Path(EPHYS_PARENT_PATH).joinpath('*')), key=os.path.getmtime)
+	pathToFile = Path(latest_subdir).joinpath('/100_CH1.continuous')
+	RAW_DATA_PATH = latest_subdir
+elif DATA_SELECTION_SCHEME == 'fromTextFileInCwd':
+	CHOSEN_REC_DIR = 'populateThisAppropriately' # 
+elif DATA_SELECTION_SCHEME == 'singleFile':
+	print()
+elif DATA_SELECTION_SCHEME == 'allInChildDirs':
+	print('processing all raw Open Ephys data in child directories of EPHYS_PARENT_PATH (use \'\' for cwd)\n')
+	pathList = []
+	if EPHYS_PARENT_PATH == '':
+		EPHYS_PARENT_PATH = os.getcwd()
+	print('processing data in: ' + EPHYS_PARENT_PATH)    
+	recPathlist = list(glob.iglob(str(Path(EPHYS_PARENT_PATH).joinpath('**/*.openephys')), recursive = True)) ### recursive file selection
+	print('\n'.join(recPathlist))
+	saveRawDataPathsToTxt(recPathlist)
+	print('\nremove some of the above from recordingIDsToRun.txt and change DATA_SELECTION_SCHEME to fromTextFileInCwd to run on a subset of the child dir data\n')
+else:
+	genericInvalidParamError()
+
+### directiory handling
+if platform.system() == 'Linux':
+	CHROME_PATH = '/usr/bin/google-chrome' # ubuntu tower path
+	print('Linux system detected\nChrome path set to: ' + CHROME_PATH)
+elif platform.system() == 'Windows':
+	CHROME_PATH = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s' # windows stim/ephys computer path	
+	DIR_OF_RECORDINGS = 'C:\\Users\\yatang\\Documents\\Ephys\\Open Ephys' # windows ephys/stim path
+	### ephys computer paths
+	pathToRawDataFile  = Path(EPHYS_PARENT_PATH).joinpath(CHOSEN_REC_DIR).with_suffix('100_CH1.continuous')
+	RAW_DATA_PATH = 'C:\\Users\\yatang\\Documents\\Ephys\\Open Ephys\\2018-05-04_13-55-49_MR1_r2_pre\\'
+	figSaveDir = 'C:\\Users\\yatang\\Documents\\Ephys\\Open Ephys\\'
+else:
+	print('WARNING: code has been optimized for Windows and Mac operating systems only.\nSet your own system parameters at the top of .py file yourself >:p')
+
+
+if INSPECT_INDIVIDUAL_CHAN ==1: # 
+	print('inspecting PSD of chan: ' + str(CHAN_NUM))
+	plt.subplot(211)
+	plt.plot(data[:,CHAN_NUM])
+	plt.subplot(212)
+	plt.psd(data[:,CHAN_NUM], 512, SAMPLE_RATE_HZ)
+	plt.show()
+elif INSPECT_INDIVIDUAL_CHAN ==0:
+	print('skipping individual channel inspection\n set INSPECT_INDIVIDUAL_CHAN to 1 for inspection and choose CHAN_NUM')
+	if SAVE_ALL_TO_PDF == 1:
+		for rawDataPath in recPathlist:
+			rawDataPath = rawDataPath.replace('/Continuous_Data.openephys','')
+			recordingName = str(os.path.basename(os.path.normpath(rawDataPath)))
+			recordingName = recordingName.replace('/','')
+			data = loadRawOEdir(rawDataPath)
+			ts = time.time()
+			timeStr = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d__%H_%M_%S')
+			pdfName = recordingName + timeStr + '.pdf'
+			numChannelsPlotted = LAST_CH - FIRST_CH + 1
+			with PdfPages(pdfName) as pdf_out:
+				for chInd in range(FIRST_CH-1, LAST_CH):
+					# f = plt.figure(figsize=(200,200)) # REDUCE THESE NUMBERS IF YOU GET SEGMENTATION/MEMORY ERRORS!
+					f = plt.figure() # REDUCE THESE NUMBERS IF YOU GET SEGMENTATION/MEMORY ERRORS!
+					plt.subplot(211)
+					plt.plot(data[:,chInd])
+					plt.subplot(212)
+					plt.psd(data[:,chInd], 512, SAMPLE_RATE_HZ)
+					if INSPECT_ONE_BY_ONE ==1:
+						print('close the plot window to advance to the next channel\nOR set INSPECT_ONE_BY_ONE to 0')
+						plt.show()
+					print('saving PSD for channel: ' + str(chInd+1) + ' of ' + str(numChannelsPlotted))
+					if chInd == LAST_CH:
+						f.suptitle(RAW_DATA_PATH, fontsize= titleFontSize)
+					pdf_out.savefig(f)
+					plt.close(f)
+					f.clf() # might help with out of memory error
+			print('finished: ' + pdfName + ' saved to cwd')
+			webbrowser.get(CHROME_PATH).open(pdfName)	
+else:
+	genericInvalidParamError('INSPECT_INDIVIDUAL_CHAN')
+
+
+
+# elif SAVE_ALL_TO_PDF == 0:
+# 	print('skipping pdf output saving step \nset SAVE_ALL_TO_PDF to 1 or output pdf')
+# else:
+# 	print('ERROR: SAVE_ALL_TO_PDF must be 0 or 1')
+
+
+
+# ### FIGURE STYLING:
+# titleFontSize = 120
+# LINE_WIDTH = 0.5
+# style.use('fivethirtyeight')
+# sns.set_style('white')
+# axes_font_size = 100
+# axes_font_weight = 'demi'
+# x_axes_font = {'fontsize': axes_font_size ,
+#                'fontweight': axes_font_weight,
+#                'verticalalignment': 'top',
+#                'horizontalalignment': 'center'}
+# y_axes_font = {'fontsize': axes_font_size ,
+#                'fontweight': axes_font_weight,
+#                'verticalalignment': 'bottom',
+#                'horizontalalignment': 'center'}
+# figTitle = 'num samples, num channels = ' + str(np.shape(data))
+# plt.rc('xtick', labelsize=32)
+# plt.rc('ytick',labelsize=32)
+# ###
+
+# X_TICK_STEP = 0.050 # in same units as time
+# Y_TICK_STEP = 100 # in microvolts (Mu agrees that it should be in microV)
+
+# ### currently only works for a second at a time
+# FIRST_SEC_PLOTTED = 1 # set to 0 to begin at the first sample
+# LAST_SEC_PLOTTED = 1.25 # set to -1 if you want to plot to the last sample 
+
+# if LAST_SEC_PLOTTED == -1:
+# 	sample_range_to_plot = [FIRST_SEC_PLOTTED*SAMPLE_RATE_HZ, totalNumSamples] # specify as a 2 int list [first sample included, last sample included]
+# elif LAST_SEC_PLOTTED > 0:
+# 	sample_range_to_plot = [round(FIRST_SEC_PLOTTED*SAMPLE_RATE_HZ), round(SAMPLE_RATE_HZ*(LAST_SEC_PLOTTED))] # specify as a 2 int list [first sample included, last sample included]
+# else:
+# 	print('ERROR: invalid LAST_SEC_PLOTTED parameter (must be -1 to run until the end or greater than 0')
+# 	sys.exit() 
+
+# numSamples = sample_range_to_plot[-1] - sample_range_to_plot[0]
+
+# # SAMPLES_PER_PAGE = 30000 #this gives proper xticks and shows 1 sec per page
+# SAMPLES_PER_PAGE = 5000 
+
+# X_LABEL = 'seconds'
+# Y_LABEL = 'microvolts'
+# # xs = xs = np.linspace(0,1,np.shape(data)[0]) 
+
+# ############## WIP don't forget to rescale to get correct units
+
+# print('sample range to plot: ' + str(sample_range_to_plot))
+
+# sampleInds = np.asarray(range(sample_range_to_plot[0],sample_range_to_plot[1]))
+# # sampleTimePts = sampleInds/SAMPLE_RATE_HZ
+# sampleTimePts = sampleInds/SAMPLE_RATE_HZ
+# print('plotted time points: ' + str(sampleTimePts[0]) + ' to ' + str(sampleTimePts[-1]))
+
+# numPages = math.ceil(numSamples/SAMPLES_PER_PAGE)
+# subplot_dim = (LAST_CH-FIRST_CH+1, 1)
+# # numXticks = -999 # temporary value designed to create an error if not handled properly within the loop 
+
+# ts = time.time()
+# timeStr = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d__%H_%M_%S')
+# pdfName = 'rawTraces' + timeStr + '.pdf'
+
+# with PdfPages(pdfName) as pdf_out:
+
+# 	for pageInd in range(1,numPages+1):
+# 	# for pageInd in range(1,numPages+1):
+# 	# for pageInd in range(1,numPages-1):
+# 		f = plt.figure(figsize=(200,200)) # REDUCE THESE NUMBERS IF YOU GET SEGMENTATION/MEMORY ERRORS!
+		
+# 		firstInd = (pageInd-1)*SAMPLES_PER_PAGE
+# 		lastInd = pageInd*SAMPLES_PER_PAGE
+
+# 		# print('firstInd: ' + str(firstInd))
+# 		# print('lastInd: ' + str(lastInd))
+
+# 		xs = sampleTimePts[firstInd:lastInd]
+
+# 		min_xs = xs[0]
+# 		max_xs = xs[-1]
+
+# 		if pageInd == 1:
+# 			numXticks = int(round((max_xs - min_xs)/X_TICK_STEP))
+
+# 		for chInd in range(FIRST_CH-1,LAST_CH):
+# 			print('plotting channel:' + str(chInd+1) + '\npage num: ' + str(pageInd) + ' of ' + str(numPages) + ' total')
+
+# 			ys = data[firstInd:lastInd,chInd]
+
+# 			sp = plt.subplot2grid(subplot_dim, (chInd, 0), aspect="auto",adjustable='box-forced',  colspan=1, rowspan=1)
+# 			sp.autoscale(True)	
+			
+# 			# ax = plt.gca() # to get current axis
+
+# 			try:
+# 				sp.plot(xs,ys,linewidth=LINE_WIDTH)
+# 			except:
+# 				print('ERROR: the number of samples can not be less than the value specified in SAMPLES_PER_PAGE')
+# 				print('WIP: this error can probably be handled by explicitly processing the remainder of samples')
+# 				### 
+# 				# sys.exit() # to halt execution upon error
+# 				continue # to disregard and continue to plot
+# 				###
+
+# 			plt.xticks(np.linspace(min_xs,max_xs,num=numXticks)) 
+
+# 		f.tight_layout() # makes the figure fill the whole window
+# 		f.subplots_adjust(hspace=0.2, left=0.1, right = 0.9, bottom = 0.05, top = 0.95)
+# 		f.suptitle(RAW_DATA_PATH, fontsize= titleFontSize)
+
+# 		plt.xlabel(X_LABEL, x_axes_font)
+# 		plt.ylabel(Y_LABEL, y_axes_font)
+
+# 		print('saving page: ' + str(pageInd))
+# 		pdf_out.savefig(f)
+		
+# 		f.clf() # might help with out of memory error
+
+# print('finished: rawTraces.pdf saved to cwd')
+
+# webbrowser.get(CHROME_PATH).open(pdfName)
+
+# plt.subplot_tool() # creates a gui for window border spacing etc
+
+# create subplots
+# sp1 = plt.subplot2grid(subplot_dim, (0, 0), aspect="auto",adjustable='box-forced', colspan=3, rowspan=2)
+# sp1 = plt.subplot2grid(subplot_dim, (0, 0), aspect="auto",adjustable='box-forced', colspan=1, rowspan=1)
+# sp = plt.subplot2grid(subplot_dim, (0, chInd), aspect="auto",adjustable='box-forced', sharex='col', sharey='row')
+# spn = plt.subplot2grid(subplot_dim, (chInd, 0), aspect="auto",adjustable='box-forced', sharex=sp1, sharey=sp1)
+
+# ###works ish
+# fig, ax = plt.subplots(64,1, figsize=(10,100),sharex='col', sharey='row', squeeze=True)
+
+# fig.tight_layout()
+
+# ax[chInd].plot(xs, ys, linewidth = 0.7, linestyle = '--')
+
+# ax = fig.add_subplot(65,1,chInd+1)	
+
+# ax = fig.add_subplot(111)
+# plt.show()
+# # plt.savefig(figSaveDir + figTitle, bbox_inches='tight', pad_inches=0.003)
+# plt.close(fig)
