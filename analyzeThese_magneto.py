854e62bfccd021b28809908dcf84264507dde121
Matthew Rosenberg <mhrosenberg@caltech.edu>
2018-07-10 14:20:01 -0700

    analyzeThese_magneto.py now capable of batch spikesorting on a directory

--- analyzeThese_magneto.py
+++ analyzeThese_magneto.py
@@ -1,17 +1,26 @@
-import matlab.engine # conflicts with conda therefore imported below
+import matlab.engine 
 import glob
-
 import os.path
 import os
-import bashrcMod # don't forget that importing a file executes it!
+# import bashrcMod # don't forget that importing a file executes it!
+import pdb
+import time
+import datetime
+import sys
 # import reinstateCondaPathToBashrc
 # import removeCondaPathFromBashrc
 
-### temp parameters DO NOT POPULATE!
-# rawMDApath = ''
-# prePath = ''
-# PATH_a_Analysis = ''
-# rawMDApaths = ''
+### CHECK THESE PARAMETERS!!!!!!
+SPIKESORTER = 'kiloSort' # use 'kiloSort', mountainSort', 'none', or 'both'
+SORT_ALL_IN_SUBDIRS = 'yes' # 'yes' or 'no' # no requires a pre-populated recordingIDsToRun.txt file and a rawDataPaths to match it in the cwd of this script 
+PATH_a_Analysis = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis/'
+PATH_rawParent = os.getcwd()
+
+## kiloSort specific parameters:
+KILOSORT_PARAMS_DIR = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis/spikeSorting/kiloSorting/' #make this file using createChannelMapFile.m
+KILOSORT_RESULTS_DIR = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Data/spikesorting/kiloSorted'
+POSTHOC_MERGE = 'no' # 'yes' or 'no' yes merges together clusters kilosort suspects as being the same etc
+OPEN_PHY_FOR_MANUAL_VERIFICATION = 'yes'
 ###
 
 def removeCondaPathFromBashrc(BASHRC_DIR):
@@ -29,32 +38,36 @@ def reinstateCondaPathInBashrc(BASHRC_DIR):
 	print(BASHRC_DIR + '.bashrc modified to reinstate conda path as follows: ')
 	print(reinstateCondaPathInBashrc)
 
+def moveKiloResultsOutOfRawDir():
+	FILES_TO_MOVE_PATH = os.getcwd() # WARNING: probably doesn't support changes to this at present!
+	ts = time.time()
+	timeStr = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d__%H_%M')
+	newSortingDirName = os.path.basename(os.getcwd()) + timeStr
+	fullDestinationPath = KILOSORT_RESULTS_DIR + '/' + newSortingDirName
+	if not os.path.exists(fullDestinationPath):
+		os.makedirs(fullDestinationPath)
+	os.system('mv ' + FILES_TO_MOVE_PATH + '/kilosort.dat ' + fullDestinationPath)
+	os.system('mv ' + FILES_TO_MOVE_PATH + '/*.npy ' + fullDestinationPath)
+	os.system('mv ' + FILES_TO_MOVE_PATH + '/params.py ' + fullDestinationPath)
+	os.system('mv ' + FILES_TO_MOVE_PATH + '/rez.mat ' + fullDestinationPath)
+
 ### recursive search 
-### stores results in pathList (don't forget to clear results in pathList before reusing)
 def recursiveSearch(exten): 
-	# The top argument for walk. Th
-	topdir = '.'
-	# Start the walk
-	os.path.walk(topdir, step, exten)
+	pathList = []
+	cwd = os.getcwd()
+	for path in glob.iglob(cwd + '/**/*' + exten, recursive=True):
+		# print(path)
+		pat = path
+		pathList.append(path)
 	return pathList
 
-def step(ext, dirname, names):
-	ext = ext.lower()
-	for name in names:
-		if name.lower().endswith(ext):
-			tmpPath = os.path.join(dirname, name) 
-			pathList.append(tmpPath)
-### end method group
-
 def saveRawDataPathsToTxt(pathList):
-	pathFile = open('rawDataPaths.txt', 'w')
-	for path in pathList:
-		cwd = os.getcwd()
-		# path = path[]
-		path = path.replace(path[:2],cwd + '/')
-		path = path.replace('/Continuous_Data.openephys','')
-		pathFile.write("%s\n" % path)
-	pathFile.close()     
+	with open('rawDataPaths.txt', 'w') as pathFile:
+		for path in pathList:
+			# cwd = os.getcwd()
+			# path = path.replace(path[:2],cwd + '/')
+			path = path.replace('/Continuous_Data.openephys','')
+			pathFile.write("%s\n" % path)
 
 def generateRecordingIDsToRun():
 	recIDlist = []
@@ -65,10 +78,9 @@ def generateRecordingIDsToRun():
 			recID = recording[slashInd+1:]
 			recIDlist.append(recID)
 
-	recIDlistFile = open('recordingIDsToRun.txt', 'w')
-	for recID in recIDlist:
-		recIDlistFile.write("%s\n" % recID)
-	recIDlistFile.close()
+	with open('recordingIDsToRun.txt', 'w') as recIDlistFile:
+		for recID in recIDlist:
+			recIDlistFile.write("%s\n" % recID)
 	return recIDlist
 
 def convertOEtoMDA():
@@ -80,7 +92,7 @@ def convertOEtoMDA():
 		recIDs = recIDs.read().splitlines()
 		rawPaths = rawPaths.read().splitlines()
 		for ind in range(0,len(recIDs)):
-			saveLocation = '../../spikesorting/mountainSorted/' + recIDs[ind].strip() #+
+			saveLocation = '../../spikesorting/mountainSorted/' + recIDs[ind].strip() #+ ############################## SUSPECT: PROBABLY A BROKEN PATH
 			if not os.path.exists(saveLocation):
 				os.makedirs(saveLocation)
 				
@@ -112,6 +124,12 @@ def getRawMDApaths():
 	return rawMDApaths
 
 def runMntSrt(rawMDApaths):
+
+
+	## mountainSort specific parameters
+	CHAN_NUMS = 64
+	PROBE_GEOM_CSV_NAME = 'geom64F' # DO NOT ADD THE .csv file ext ############################# TO DO: get the geometry from the text file
+
 	# removeCondaPathFromBashrc() # anaconda path in bashrc breaks mountainSort but is necessary for phy (kilosort visualization)
 	os.chdir(PATH_rawParent)
 	print('cd into ' + os.getcwd())
@@ -157,106 +175,114 @@ def runMntSrt(rawMDApaths):
 
 def runKiloSort():
 	os.chdir(PATH_rawParent)
+	print('starting matlab engine for kilosorting')
 	eng = matlab.engine.start_matlab()
-	with open('recordingIDsToRun.txt') as recIDs, open('rawDataPaths.txt') as rawPaths:
-		recIDs = recIDs.read().splitlines()
-		rawPaths = rawPaths.read().splitlines()
-		print('\nPreparing to sort: ')
-		print('\n'.join(recIDs))
-		print('\namong the following (possible) raw data in subdirs of the cwd: ')
-		print('\n'.join(rawPaths))
+	print('engine running')
+	try:
+		with open('recordingIDsToRun.txt') as recIDs, open('rawDataPaths.txt') as rawPaths:
+			recIDs = recIDs.read().splitlines()
+			rawPaths = rawPaths.read().splitlines()
+			print('\nPreparing to sort: ')
+			print('\n'.join(recIDs))
+			print('\namong the following (possible) raw data in subdirs of the cwd: ')
+			print('\n'.join(rawPaths))
+	except:
+		sys.exit("recordingIDsToRun.txt and rawDataPaths.txt missing from cwd: run code again with SORT_ALL_IN_SUBDIRS set to yes\n(set SPIKESORTER to none if you wish to modify the text files before sorting all child recordings in cwd)")
 		
-		for recID in recIDs:
-			for rawPath in rawPaths:
-				if recID in rawPath:
-					os.chdir(PATH_rawParent)
-					try:
-						os.chdir('../../spikesorting/kiloSorted')
-					except:
-						print('\n dir not found, therefore creating dir: ../../spikesorting/kiloSorted')
-						os.chdir('../../')
-						os.system('mkdir spikesorting/kiloSorted') # UNTESTED: can this make a nested directory?
-					kiloDir = os.getcwd()
-					kiloSaveDir = kiloDir + '/' + recID
-					if not os.path.exists(kiloSaveDir):
-						os.chdir(rawPath)
+	for recID in recIDs:
+		for rawPath in rawPaths:
+			if recID in rawPath: # nice implicit string search here
 
-						if not os.path.exists('./100_CH1.continuous'):
-							os.system('cp 100_ch1.continuous ./100_CH1.continuous') # alternatively: os.system("rename 's/ch/CH/' *.continuous")
-							print('copied continuous OE file from ch1 to CH1')
-						else:
-							print('data already named correctly; no need to rename OE file from ch1 to CH1')
-						os.chdir(PATH_rawParent)
-						os.chdir('../../spikesorting/a_paramsNconfigs/kiloSorting/')
-						eng.addpath(os.getcwd())
+				### format output results directory
+				try:
+					os.chdir(KILOSORT_RESULTS_DIR)
+				except:
+					print('\n dir not found, therefore creating dir: ' + KILOSORT_RESULTS_DIR)
+					os.system('mkdir ' + KILOSORT_RESULTS_DIR) # UNTESTED: can this make a nested directory?					
+				newKiloSaveDir = KILOSORT_RESULTS_DIR + '/' + recID
+				
+				
+				if POSTHOC_MERGE.lower() == 'yes':
+					newKiloSaveDir = newKiloSaveDir + 'posthocMerged'
+				elif POSTHOC_MERGE.lower() != ('yes' and 'no'):
+					sys.exit("\n\nPOSTHOC_MERGE param invalid! must be yes or no\n\n")
 
-						if os.path.exists(PROBE_CHAN_MAP_PATH):
-							eng.workspace['CHAN_MAP_MAT_PATH'] = PROBE_CHAN_MAP_PATH
-						else:
-							print('ERROR: ' + PROBE_CHAN_MAP_PATH + 'does not exist')
-						print('reminder: have you checked that the parameters in the separate matlab files are correct? \nLook in: ' + PROBE_CHAN_MAP_PATH)
+				if not os.path.exists(newKiloSaveDir):
+					os.chdir(rawPath)
 
-						eng.workspace['RAW_DATA_ROOT_PATH'] = rawPath.strip()
-
-						if POSTHOC_MERGE == 'yes':
-							eng.master_file_2_9_18posthocMerged(nargout = 0)
-							kiloSaveDir = kiloSaveDir + 'posthocMerged'
-						elif POSTHOC_MERGE == 'no':
-							eng.master_file_2_5_18(nargout = 0)
-						
-						os.makedirs(kiloSaveDir)
-						os.system('mv ' + rawPath + '/kilosort.dat ' + kiloSaveDir)
-						os.system('mv ' + rawPath + '/*.npy ' + kiloSaveDir)
-						os.system('mv ' + rawPath + '/params.py ' + kiloSaveDir)
-						os.system('mv ' + rawPath + '/rez.mat ' + kiloSaveDir)
+					### rename first OE file if it has the weird capitalization
+					if not os.path.exists('./100_CH1.continuous'):
+						os.system('cp 100_ch1.continuous ./100_CH1.continuous') # alternatively: os.system("rename 's/ch/CH/' *.continuous")
+						print('copied continuous OE file from ch1 to CH1')
 					else:
-						print('\nsorting results already exist for: ' + recID)
-						print('rename, move, or delete this dir to rerun the analysis of this dataset')
-	print('\nfinished with kiloSorting')
+						print('data already named correctly; no need to rename OE file from ch1 to CH1')
+					
+					# read probe type from probeType.txt
+					with open('probeType.txt') as f:
+						probeType = f.readline()
+						probeType = probeType.strip()
+					print('using chanmap for probe: ' + probeType)
+
+					### run the sorting
+					print('now kilosorting: ' + rawPath + '\n\nJust be patient now... :)')
+					os.chdir(KILOSORT_PARAMS_DIR)
+					eng.addpath(os.getcwd())
+					eng.workspace['RAW_DATA_ROOT_PATH'] = rawPath.strip()
+
+					if probeType.lower() == '64f':
+						if POSTHOC_MERGE.lower() == 'yes':
+							eng.master_file_2_9_18posthocMerged_64F(nargout = 0)
+						elif POSTHOC_MERGE.lower() == 'no':
+							eng.master_file_6_12_18_64F(nargout = 0)
+					elif probeType.lower() == '64g':
+						if POSTHOC_MERGE.lower() == 'yes':
+							eng.master_file_2_9_18posthocMerged_64G(nargout = 0)
+						elif POSTHOC_MERGE.lower() == 'no':
+							eng.master_file_6_12_18_64G(nargout = 0)								
+					else:
+						sys.exit('ERROR: chanmap unavailable for that probe type or probeType.txt missing from raw data dir')
+
+					os.chdir(rawPath)
+					moveKiloResultsOutOfRawDir()
+
+				else:
+					print('\nsorting results already exist for: ' + newKiloSaveDir)
+					print('rename, move, or delete this dir to rerun the analysis of this dataset')
+				print('\nfinished with kiloSorting: ' + recID)
+
+def openPhyForManualSorting():
+
+	
 
 ### MAIN CODE: 
 
-## common parameters:
-CHAN_NUMS = 64
-SPIKESORTER = 'mountainSort' # use 'kiloSort', mountainSort', 'none', or 'both'
-SORT_ALL_IN_SUBDIRS = 'yes' # 'yes' or 'no' # no requires a pre-populated recordingIDsToRun.txt file in the cwd of this script 
-PATH_a_Analysis = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis'
-PATH_rawParent = os.getcwd()
-BASHRC_DIR = '/home/matthew'
-
-## kiloSort specific parameters:
-PROBE_CHAN_MAP_PATH = '/media/matthew/Data/a_Ephys/a_Projects/a_Magnetoreception/a_Analysis/spikeSorting/kiloSorting/probe64F_chanMap.mat' #make this file using createChannelMapFile.m       
-POSTHOC_MERGE = 'no' # 'yes' or 'no' yes merges together clusters kilosort suspects as being the same etc
-
-## mountainSort specific parameters
-PROBE_GEOM_CSV_NAME = 'geom64F' # DO NOT ADD THE .csv file ext
-
 ### CHOOSING DATA: output = recordingIDsToRun.txt
 ## get all data paths for subdirs in ./
-pathList = []    
-if SORT_ALL_IN_SUBDIRS == 'yes':
+# pathList = []    
+if SORT_ALL_IN_SUBDIRS.lower() == 'yes':
 	print('sorting all in subdirs from the location of this script')
 	exten = '.openephys' # make this a UNIQUE FILE within the raw open ephys dir
 
-	recursiveSearch(exten)
+	pathList = recursiveSearch(exten)
+	print(pathList)
 	saveRawDataPathsToTxt(pathList)
 	### comment out and modify recordingIDsToRun.txt manually to run on a subset of the raw data in the child dirs of this directory
 	recIDlist = generateRecordingIDsToRun() 
 	print('\n'.join(recIDlist))
 	print('\nremove some of the above from recordingIDsToRun.txt to run on a subset of the child dir data\n')
-elif SORT_ALL_IN_SUBDIRS == 'no':
-  print('sorting based on contents of recordingIDsToRun.txt')
-#   ## WIP check for file existence
-else:
-	print('error: invalid selection for SORT_ALL_IN_SUBDIRS param')
+elif SORT_ALL_IN_SUBDIRS.lower() == 'no':
+	if os.path.exists('recordingIDsToRun.txt'):
+		print('sorting based on contents of recordingIDsToRun.txt')
+	else:
+		print('error: invalid selection for SORT_ALL_IN_SUBDIRS param')
 
 
 ### SPIKE SORTING
 print('using spikesorter: ' + SPIKESORTER) ########## WIP: programmatically solve conda bashrc conflict b/w kilosort and mntSort
-if SPIKESORTER == 'kiloSort':
+if SPIKESORTER.lower() == 'kilosort':
 	print('spikesorting with kilosort')
 	runKiloSort()
-elif SPIKESORTER == 'mountainSort':
+elif SPIKESORTER.lower() == 'mountainsort':
 	### FILE TYPE CONVERSION: only needs to be run the first time
 	convertOEtoMDA()
 	## searches through all MDA files in this project dir tree for matches with recordingIDsToRun.txt
@@ -266,7 +292,7 @@ elif SPIKESORTER == 'mountainSort':
 	print('\n'.join(rawMDApaths))
 
 	runMntSrt(rawMDApaths) # WIP: make param for mntSrt_run_options 
-elif SPIKESORTER == 'both':
+elif SPIKESORTER.lower() == 'both':
 	#### mountainSort
 	### FILE TYPE CONVERSION: only needs to be run the first time
 	convertOEtoMDA()
@@ -278,10 +304,19 @@ elif SPIKESORTER == 'both':
 	#### kiloSort
 	print('spikesorting with kilosort')
 	runKiloSort()
-elif SPIKESORTER == 'none':
+elif SPIKESORTER.lower() == 'none':
 	print('you chose not to spikesort')
 else:
 	print('invalid sorter specified as parameter. use kiloSort or mountainSort')
 
 ### VIEW RESULTS
 # reinstateCondaPathInBashrc(BASHRC_DIR)
+
+if OPEN_PHY_FOR_MANUAL_VERIFICATION.lower() == 'yes':
+
+	### get list of prepared kilosort outputs
+
+	### navigate to kilosort dir
+
+	### run phy command
+
